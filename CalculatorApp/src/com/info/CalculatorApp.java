0f3b112b2bb0e0dab13afa71e2fe2e411419bafc
Sakshi Jain <137858377+sakshi-jain-1212@users.noreply.github.com>
2026-03-06 16:01:49 +0530

    Add files via upload
    Task 1 Simple Calculator

---
+++ CalculatorApp/src/com/info/CalculatorApp.java
@@ -0,0 +1,13 @@
+
+package com.info;
+
+import com.info.ui.MyCalcFrame;
+public class CalculatorApp {
+
+   
+    public static void main(String[] args) {
+         MyCalcFrame calc=new MyCalcFrame();
+        calc.setVisible(true); 
+    }
+    
+}
